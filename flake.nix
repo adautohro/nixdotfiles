2499a3fbfcd20d21e8454e807c6244fc010d849a
Adauto Henrique <112290445+adautohro@users.noreply.github.com>
2026-08-31 10:23:48 -0300

    basics and noctalia working

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "my NixOS Flake";
+
+  nixConfig = {
+	  extra-substituters = [ "https://noctalia.cachix.org" ];
+	  extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+	};
+ 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+     url = "github:nix-community/home-manager";
+    };
+ 
+    noctalia = {
+     url = "github:noctalia-dev/noctalia/cachix";
+    }; 
+  };
+ 
+  outputs = inputs@{ nixpkgs, home-manager, ...}: {
+   nixosConfigurations = {
+    nixos = nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit inputs; }; #Pass the inputs dependencies to all modules below
+     modules = [
+       ./configuration.nix
+       home-manager.nixosModules.home-manager
+       {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+ 	  home-manager.backupFileExtension = "backup";
+          home-manager.users.adautohro = ./home.nix; 
+       }
+     ];
+     };
+    };
+   };
+}
